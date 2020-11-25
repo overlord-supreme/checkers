aad34de970a64157219471c1e4e98569f7bb909b
Peter Mangelsdorf <pjm349@drexel.edu>
2020-11-25 12:08:05 -0500

    Add Man Asm; Add King Asm

---
+++ Assets/SupremeCheckers/King/King.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The King Piece
+/// Note: Should not have any code here
+/// </summary>
+public class King : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/SupremeCheckers/Man/Man.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// The Man Piece
+/// Note: Should not have any code
+/// </summary>
+public class Man : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
