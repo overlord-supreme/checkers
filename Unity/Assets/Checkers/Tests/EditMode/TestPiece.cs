183fd53de29f302d19b5497b28a9fa3fd421bb0a
henry-PC <henry-PC@LAPTOP-I2JD4AE8>
2020-11-29 16:14:56 -0500

    Code dump for TestPiece.cs

---
+++ Unity/Assets/Checkers/Tests/EditMode/TestPiece.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class TestPiece
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void TestPromotingPiece() 
+        {
+            var gameObject = new GameObject();
+            gameObject.AddComponent<Piece>();
+            Piece piece = gameObject.GetComponent<Piece>();
+            piece.king = new GameObject();
+            Assert.IsFalse(piece.isKing);
+            piece.promotePiece();
+            Assert.IsTrue(piece.isKing);
+        }
+    }
+}
